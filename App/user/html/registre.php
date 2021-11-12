d419fbc541bbab937729fea93526c3f10092e337
Bruno Saldaña Serrano <brsase@fp.insjoaquimmir.cat>
2021-11-12 19:04:02 +0100

    register.php moved to user

---
+++ App/user/html/registre.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Projecte J-Suite</title>
+   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+   <link href="http://localhost/tarda/project-j-suite/App/user/css/user.register.css" rel="stylesheet" type="text/css">
+   <link href="http://localhost/tarda/project-j-suite/App/_commons/css/reset.css" rel="stylesheet" type="text/css">
+   <link href="http://localhost/tarda/project-j-suite/App/_commons/css/styles.css" rel="stylesheet" type="text/css">
+   <link href="http://localhost/tarda/project-j-suite/App/_commons/css/components.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+   <main class="main">
+      <header class="header">
+         <div class="logo-wrapper">
+            <a class="link link__logo" href="">LOGO</a>
+         </div>
+         <form action="/form.php">
+            <div class="form__row2">
+               <label class="txt">Usuari:</label>
+               <input class="input">
+               <div class="check">
+                  <input type="checkbox" id="rememberpasswd" name="rememberpasswd">
+                  <label for="rememberpasswd">Recordar sesión</label>
+               </div>
+            </div>
+            <div class="form__row2">
+               <label class="txt">Contrasenya:</label>
+               <input class="input">
+               <a class="recuperarcontra" href="">Recuperar contrasenya</a>
+            </div>
+            <input class="button2" type="button" value="LOGIN"/>
+         </form>
+      </header>
+      <section class="section">
+
+      </section>
+      <footer class="footer">
+         <p>Curs 2021-22 de 2DAW</p>
+      </footer>
+   <main>
+</body>
+</html>
