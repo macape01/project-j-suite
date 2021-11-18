2537b08c1c2dcdfb5b444fac4c51fcc04589001d
OliverBocanegraPerez <olbope@fp.insjoaquimmir.cat>
2021-11-18 18:32:40 +0100

    avances m9

--- App/_commons/footer.php
+++ App/_commons/footer.php
@@ -1,4 +1,3 @@
 <footer class="footer">
     <p>Connect2gether © 2021-2022</p>
-    <!-- <script src="<?= My\Helpers::url("/_commons/js/scripts.js") ?>"></script> -->
 </footer>
--- App/_commons/head.php
+++ App/_commons/head.php
@@ -5,9 +5,6 @@
     <title>Projecte J-Suite / <?= $__params["subtitle"] ?? "" ?></title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/reset.css")?>"/>
-    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/styles.css")?>"/>
     <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/components.css")?>"/>
-    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/layout.css")?>"/>
-    <link rel="stylesheet" href="<?= My\Helpers::url("/user/css/user.register.css")?>"/>
-    <link rel="stylesheet" href="<?= My\Helpers::url("/user/css/user.register2.css")?>"/>
+    <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/home.float.css")?>"/>
 </head>
