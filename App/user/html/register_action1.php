10176f532bc58900cfc39798166e31168d5f0d81
OliverBocanegraPerez <olbope@fp.insjoaquimmir.cat>
2021-11-30 18:27:07 +0100

    f

--- App/user/html/register_action1.php
+++ App/user/html/register_action1.php
@@ -8,9 +8,10 @@ use My\Database;
 use My\Helpers;
 
 $validator = new Validator();
+$existe = false;
 
 $validation = $validator->make($_POST + $_FILES, [
-//    'username'           => 'required|min:6',
+    'username'           => 'required|min:6',
 //    'name'               => 'required',
 //    'lastname'           => 'required',
     'email'              => 'required|email',
@@ -26,27 +27,52 @@ if ($validation->fails()) {
     Helpers::redirect($url);
 } 
 else {
-    echo "Success!";
+    My\Helpers::log()->debug("Success!");
 
     $email = $_POST['email'];
-    echo $email;
+    $username = $_POST['username'];
 
-    $db = new Database();
-    $sentencia = $db->prepare("SELECT email FROM `2daw.equip01`.users WHERE email = '{$email}'");
-    $sentencia->execute();
-    foreach ($sentencia as $comprobar){
-        if($comprobar['email']==$_POST['email']){
-            $existe = true;
-            break;
+
+    try {
+        $db = new Database();
+        $sql = "SELECT email FROM `2daw.equip01`.users WHERE email = '{$email}'";
+        $sentencia = $db->prepare($sql);
+        $sentencia->execute();
+    
+        foreach ($sentencia->fetchAll() as $comprobar){
+            if($comprobar['email']==$_POST['email']){
+                $existe = true;
+                break;
+            }
         }
-    }
 
-    if ($existe == true){
-        echo "HOLA";
-    }
+        $sql = "SELECT username FROM `2daw.equip01`.users WHERE username = '{$username}'";
+        $sentencia2 = $db->prepare($sql);
+        $sentencia2->execute();
 
-    if ($existe == false){
-        echo "ADIOS";
+        foreach ($sentencia2->fetchAll() as $comprobar){
+            if($comprobar['username']==$_POST['username']){
+                $existe = true;
+                break;
+            }
+        }
+    
+        if ($existe == true){
+            My\Helpers::log()->debug("MISSATGE D'ERROR: L'usuari o email ja existeix!");
+            $url = Helpers::url("/user/html/register2.php");
+            Helpers::redirect($url);
+        }
+    
+        if ($existe == false){
+            $sql = "INSERT INTO `2daw.equip01`users (email, username, status) VALUES ('$email', '$username', 0)";
+            $sentencia3 = $db->prepare($sql);
+            $sentencia3->execute();
+            My\Helpers::log()->debug("CREADISIMO BRO");
+
+            
+        }    
+    } catch (Exception $e) {
+        My\Helpers::log()->err("ERRORRRRRRRRRRRRR");
     }
 }
 ?>
--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
- 
- return [
+
+return [
    "driver"    => "mysql",
    "host"      => "alumnes.insjoaquimmir.cat",
    "port"      => 9316,
    "database"  => "2daw.equip01",
    "user"      => "2daw.equip01",
-   "password"  => "tarda1234",
+   "password"  => "E3%yAejD",
    "options"   => [
        PDO::MYSQL_ATTR_SSL_KEY                => __DIR__ . '/client-key.pem',
        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
