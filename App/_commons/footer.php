4ed9ebe5332b88f34ab4fef97584c6b21956274a
Marc Carbonell Perez <macape@fp.insjoaquimmir.cat>
2021-12-14 20:05:16 +0100

    M09 - Bootstrap

--- App/_commons/footer.php
+++ App/_commons/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
     <p>Connect2gether © 2021-2022</p>
-    <!-- <script src="<?= My\Helpers::url("/_commons/js/scripts.js") ?>"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </footer>
--- App/_commons/head.php
+++ App/_commons/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projecte J-Suite / <?= $__params["subtitle"] ?? "" ?></title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
@@ -9,6 +10,7 @@
     <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/components.css")?>"/>
     <link rel="stylesheet" href="<?= My\Helpers::url("/_commons/css/layout.css")?>"/>
     <link rel="stylesheet" href="<?= My\Helpers::url("/user/css/user.register.css")?>"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <?php $flash = My\Helpers::flash(); ?>
