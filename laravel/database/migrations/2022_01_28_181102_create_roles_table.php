248279b984b8da8ca9ef4d92c9d6b5314ade20ae
Marc Carbonell Perez <macape@fp.insjoaquimmir.cat>
2022-01-28 19:40:27 +0100

    mail/test rute authentication needed, migration table roles not finished

---
+++ laravel/database/migrations/2022_01_28_181102_create_roles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name")->unique();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('role_id');
+            $table->dropColumn('role_id');
+        });
+        Schema::dropIfExists('roles');
+    }
+}
