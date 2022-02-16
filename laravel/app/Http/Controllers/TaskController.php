db44208d70f7af2c26341d6097b12e3dccf985a9
EduardSha <dudo@fp.insjoaquimmir.cat>
2022-02-16 20:25:37 +0100

    Task controller, modal y Test Subidisimos avanzando

---
+++ laravel/app/Http/Controllers/TaskController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = DB::table('tasks')
+        ->select('comentari', 'id', 'author_id', 'titol')
+        ->get();
+        return \response($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'comentari' => 'max:255',
+            'id' => 'required|max:50',
+            'author_id' => 'required',
+            'titol' => 'required',
+        ]);
+        $task = Task::create($request->all());
+        return \response($task);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tasks = DB::table('tasks')
+        ->select('comentari', 'id', 'author_id', 'titol')
+        ->where('id','=',$id)
+        ->get();
+        return \response($tasks);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $ttask=Ticket::find($id);
+        $task->update($request->all());
+        return $task;
+    }-
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- laravel/tests/Feature/TaskTest.php
+++ laravel/tests/Feature/TaskTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+
+
 class TaskTest extends TestCase
 {
     /**
@@ -13,10 +15,76 @@ class TaskTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_tasks_listed()
     {
-        $response = $this->get('/api/tasks');
 
+            //Responses
+            $responseStatus = $this->get('/api/tasks');
+
+            $response = $this->getJson('/api/tasks');
+            
+            //Assertions
+            $responseStatus->assertStatus(200);
+            /* $response->assertJson(fn (AssertableJson $json) =>
+            $json->hasAll('id', 'description', 'title', 'assigned_id', 'asset_id')
+            ); */
+    }
+
+    public function test_task_can_be_created()
+    {
+        //Mock data ticket
+
+        $task_data = [
+            'comentari' => 'jeepeta',
+            'id' => 1,
+            'author_id' => 1,
+            'titol' => 'bichota',
+        ];
+
+        //Responses
+        $response = $this->postJson('/api/tasks', $task_data);
+
+        //Assertions
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);
+
+    }
+    public function test_task_can_be_retrieved()
+    {
+        //Responses
+        $response = $this->get('/api/tasks/1');
+
+        //Assertions
         $response->assertStatus(200);
+
+    }
+    public function test_task_can_be_updated()
+    {
+        //Mock data ticket
+
+        $task_data = [
+            'titol' => 'cosas',
+            'comentari' => 'cositas',
+        ];
+
+        //Responses
+        $response = $this->put('/api/tasks/1', $task_data);
+
+        //Assertions
+        $response->assertStatus(200);
+
+    }
+    public function test_task_can_be_deleted()
+    {
+
+        //Responses
+        $response = $this->delete('/api/tasks/1');
+
+        //Assertions
+        $response->assertStatus(200);
+
     }
 }
