d6c2dd2ef7fbe22582d1a618ff75104b044affdc
OliverBocanegraPerez <olbope@fp.insjoaquimmir.cat>
2022-02-16 20:54:45 +0100

    mamuten

---
+++ laravel/app/Http/Controllers/ChatController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $message = DB::table('Messages')
+        ->select('id', 'author_id', 'message', 'created', 'publicgroup_id', 'privateuser_id')
+        ->get();
+        return \response($message);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id' => 'required',
+            'author_id' => 'required',
+            'message' => 'required',
+            'created' => 'required',
+            'publicgroup_id' => 'required',
+            'privateuser_id' => 'required',
+        ]);
+        $message = Messages::create($request->all());
+        return \response($message);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $message = DB::table('Messages')
+        ->select('id', 'author_id', 'message', 'created', 'publicgroup_id', 'privateuser_id')
+        ->where('id','=',$id)
+        ->get();
+        return \response($message);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $message=Messages::find($id);
+        $message->update($request->all());
+        return $message;
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Messages::destroy($id);
+    }
+}
--- laravel/tests/Feature/ChatTest.php
+++ laravel/tests/Feature/ChatTest.php
@@ -13,10 +13,74 @@ class ChatTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_messages_can_be_listed()
     {
-        $response = $this->get('/api/chats');
+        //Responses
+        $responseStatus = $this->get('/api/Messages');
 
+        $response = $this->getJson('/api/Messages');
+        
+        //Assertions
+        $responseStatus->assertStatus(200);
+        /* $response->assertJson(fn (AssertableJson $json) =>
+        $json->hasAll('id', 'description', 'title', 'assigned_id', 'asset_id')
+        ); */
+    }
+    public function test_messages_can_be_created()
+    {
+        //Mock data ticket
+
+        $message = [
+            'id' => 1,
+            'author_id' => 3,
+            'message' => 'hola que tal',
+            'created' => '2/2/22',
+            'publicgroup_id' => 5,
+            'privateuser_id' => null,
+        ];
+
+        //Responses
+        $response = $this->postJson('/api/Messages', $message);
+
+        //Assertions
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);
+    }
+    public function test_messages_can_be_retrieved()
+    {
+        //Responses
+        $response = $this->get('/api/Messages/1');
+
+        //Assertions
         $response->assertStatus(200);
+
+    }
+    public function test_messages_can_be_updated()
+    {
+        //Mock data ticket
+
+        $message = [
+            'message' => 'esto es un hola dos',
+        ];
+
+        //Responses
+        $response = $this->put('/api/Messages/1', $message);
+
+        //Assertions
+        $response->assertStatus(200);
+
+    }
+    public function test_messages_can_be_deleted()
+    {
+
+        //Responses
+        $response = $this->delete('/api/Messages/1');
+
+        //Assertions
+        $response->assertStatus(200);
+
     }
 }
